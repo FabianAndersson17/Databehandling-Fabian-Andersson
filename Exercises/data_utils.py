fa742798b936a5448007f76f5dc1dbe1d22ff529
FabianAndersson17 <89392496+FabianAndersson17@users.noreply.github.com>
2021-10-20 14:32:36 +0200

    Tried a few diffrent ways to make the plot work. Workin progress

--- Exercises/data_utils.py
+++ Exercises/data_utils.py
@@ -1,5 +1,7 @@
 import seaborn as sns
+import matplotlib.pyplot as plt
 import pandas as pd
 
+fig, ax = plt.subplots(1, 2, )
 def Plot_Missing_Data(dataFrame):
-    sns.barplot(data=dataFrame, x=dataFrame.isnull(), y=dataFrame.isnull().nunique())
+    sns.barplot(data=dataFrame, x=dataFrame.isnull(by="rows"), y=dataFrame.isnull().nunique())
