00e6d0873075aa65e7e7dab8309c146559924bb3
FabianAndersson17 <89392496+FabianAndersson17@users.noreply.github.com>
2021-11-02 09:48:40 +0100

    Stared making a stock dash board

--- Code-alongs/L5-stockdash/L5.2-dashboard/main.py
+++ Code-alongs/L5-stockdash/L5.2-dashboard/main.py
@@ -1,1 +1,15 @@
 import pandas as pd
+from dash import dcc, html
+import dash
+
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.H1("Stocks viewer"),
+    html.H2("This is a cool app")
+
+])
+
+if __name__ == "__main__":
+    app.run(debug=True)
